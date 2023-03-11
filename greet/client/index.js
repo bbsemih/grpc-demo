9a1662ec7448161baad9905a7dc482d6837b2115
Semih Berkay Öztürk <90466553+semihberkayozturk@users.noreply.github.com>
2023-03-11 22:30:17 +0300

    complete greet api

--- greet/client/index.js
+++ greet/client/index.js
@@ -1,13 +1,26 @@
-const grpc = require("@grpc/grpc-js");
+const grpc = require("@grpc/grpc-js")
+const { GreetServiceClient } = require("../proto/greet_grpc_pb.js");
+const { GreetRequest } = require("../proto/greet_pb.js");
 
-const { GreetServiceClient } = require("../proto/greet_grpc_pb");
+function doGreet(client) {
+    console.log("doGreet was invoked!");
+    const req = new GreetRequest()
+        .setFirstName("Semih")
+    client.greet(req, (err, res) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(`Greet: ${res.getResult()}`);
+    })
+}
+
+//To use a gRPC client to communicate with a gRPC server. 
 
 function main() {
     const creds = grpc.ChannelCredentials.createInsecure();
     const client = new GreetServiceClient("localhost:50051", creds);
 
-    //...
-
+    doGreet(client)
     client.close();
 }
 
--- greet/server/index.js
+++ greet/server/index.js
@@ -1,30 +1,38 @@
 const grpc = require("@grpc/grpc-js");
+const serviceImpl = require("./service_impl");
+const { GreetServiceService } = require("../proto/greet_grpc_pb.js");
 
-const addr = 'localhost:50051';
+const addr = "localhost:50051";
 
 function cleanup(server) {
-    console.log("Cleanup");
     if (server) {
-        server.forceShutdown();
+        console.log("Cleanup...");
+        if (server) {
+            server.forceShutdown();
+        }
     }
-};
+}
 
 function main() {
     const server = new grpc.Server();
     const creds = grpc.ServerCredentials.createInsecure();
 
+    //When you press CTRL+C
     process.on("SIGINT", () => {
-        console.log("Caught interrupt signal");
+        console.log("Caught interrupt signal!");
         cleanup(server);
     })
 
+    server.addService(GreetServiceService, serviceImpl);
+    //Binds the server to the specified address 
+    //and port number and starts listening for incoming requests
     server.bindAsync(addr, creds, (err, _) => {
         if (err) {
-            return cleanup(server)
+            return cleanup(server);
         }
         server.start();
     })
-    console.log(`Listening on port ${addr}`);
+    console.log(`Server listening on port: ${addr}`);
 };
 
 main();
