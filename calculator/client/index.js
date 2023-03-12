ce610b2fbfce8edc498a05057d89dbffcb1fe8b3
Semih Berkay Öztürk <90466553+semihberkayozturk@users.noreply.github.com>
2023-03-12 22:35:02 +0300

    implement client streaming for calc

--- calculator/client/index.js
+++ calculator/client/index.js
@@ -2,6 +2,7 @@ const grpc = require("@grpc/grpc-js")
 const { CalculatorServiceClient } = require("../proto/calculator_grpc_pb");
 const { SumRequest } = require("../proto/sum_pb");
 const { PrimeRequest } = require("../proto/primes_pb");
+const { AverageRequest } = require("../proto/average_pb");
 
 function doSum(client) {
     console.log("doSum was invoked!");
@@ -31,14 +32,29 @@ function doPrimes(client) {
     })
 }
 
-//To use a gRPC client to communicate with a gRPC server. 
+function doAverage(client) {
+    console.log("doAverage was invoked!");
+    const numbers = [...Array(11).keys()].slice(1);
+    const call = client.average((err, res) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(`Average: ${res.getResult()}`);
+    })
+    numbers.map((num) => {
+        return new AverageRequest().setNumber(num);
+    }).forEach((req) => call.write(req));
+    call.end();
+};
 
+//To use a gRPC client to communicate with a gRPC server. 
 function main() {
     const creds = grpc.ChannelCredentials.createInsecure();
     const client = new CalculatorServiceClient("localhost:50051", creds);
 
     //doSum(client)
-    doPrimes(client)
+    //doPrimes(client)
+    doAverage(client)
     client.close();
 }
 
